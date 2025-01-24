7aeb6a71d2815d29ed5ada5a3cc83d7fb6c1a347
christopher-moeller <35485851+christopher-moeller@users.noreply.github.com>
2025-01-24 15:47:08 +0100

    refactoring of ImGui setup

--- Engine/src/Harmony/ImGui/ImGuiLayer.cpp
+++ Engine/src/Harmony/ImGui/ImGuiLayer.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 
 #include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
 #include "Harmony/Application.h"
@@ -19,18 +20,26 @@ namespace Harmony {
 
     void ImGuiLayer::OnAttach() {
         
+        // Setup ImGui context
+        IMGUI_CHECKVERSION();
         ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
+
+        // Setup ImGui style
         ImGui::StyleColorsDark();
 
-        ImGuiIO& io = ImGui::GetIO();
-        io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
-        io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
-        
+        Application& app = Application::Get();
+        GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
+        // Initialize ImGui backends
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init("#version 410");
     }
 
     void ImGuiLayer::OnDetach() {
-
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
     }
 
     void ImGuiLayer::OnUpdate() {
@@ -44,6 +53,7 @@ namespace Harmony {
         m_Time = time;
 
         ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
         static bool show = true;
